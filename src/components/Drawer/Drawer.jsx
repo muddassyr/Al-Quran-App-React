6d5207b14ca3db1e69bf7f0209fe9dcb44873d2a
muddassirk <mkkhanu72@gmail.com>
2021-05-13 18:45:07 +0500

    some changes in drawer

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -83,6 +83,9 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  ulClass: {
+    marginRight: 0,
+  },
 }));
 
 export default function LeftDrawer() {
@@ -152,7 +155,7 @@ export default function LeftDrawer() {
           ))}
         </List>
         <Divider /> */}
-        <List>
+        <List className={classes.ulClass}>
           <ListItem>
             <ListItemIcon>
               <AudiotrackIcon />
@@ -170,18 +173,7 @@ export default function LeftDrawer() {
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-      
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+    
       </main>
     </div>
   );
