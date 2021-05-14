c4ec87e18e2ec0b1f057e25bf6506306e5d72ef2
muddassirk <mkkhanu72@gmail.com>
2021-05-15 00:05:01 +0500

    fetch api  and add Link

--- src/components/QuraanEkareem/QuraanEKareem.jsx
+++ src/components/QuraanEkareem/QuraanEKareem.jsx
@@ -4,6 +4,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import '../css/style.css'
+import {
+  Link
+} from "react-router-dom";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -45,32 +49,38 @@ export default function QuraanEKareem() {
     axios({
       method: 'get',
       // url: 'http://api.alquran.cloud/v1/edition', //quran editon
-      url: 'http://api.alquran.cloud/v1/surah', // get only surah
+      // url: 'http://api.alquran.cloud/v1/surah', // get only surah
       // url: 'http://api.alquran.cloud/v1/juz/{{juz}}/{{edition}}', // get a juz
       // url: 'http://api.alquran.cloud/v1/search/{{keyword}}/{{surah}}/{{edition or language}}', // search all ayah, surah
-      // url: 'http://api.alquran.cloud/v1/quran/{{edition}}',//complete quran
+      url: 'http://api.alquran.cloud/v1/quran/{{edition}}',//complete quran
       // url: 'http://api.alquran.cloud/v1/page/{{page}}/{{edition}}',//get a page no
       // url: 'http://api.alquran.cloud/v1/meta',//Meta - Get meta data about Surahs, Pages, Hizbs and Juzs
     }).then((response) => {
       // console.log(response, 'responese');
       // console.log(response.data.data);
-      // console.log((response.data.data));
-      setData(response.data.data)
+      console.log((response.data.data.surahs));
+      setData(response.data.data.surahs)
     })
   }, [])
 
 
 
   return (
-    <div className={classes.root} className='mediaScreen'>
+    <div className={classes.root}>
       {/* <Grid container spacing={3}> */}
       <Grid item xs={12}>
         <Paper className={classes.paperBackGround} >Surah Details</Paper>
       </Grid>
+      <hr />
       {data.map((value, index) => {
         // console.log(value);
         return (
-          <div key={index}>
+          <Link 
+            key={index}
+            className=''
+            to={`/quranekareem/${index}`}
+          >
+          <div >
             <Grid item xs={12}>
               <Paper className={classes.paper}>
                 <div className={classes.mapInside}>
@@ -88,10 +98,13 @@ export default function QuraanEKareem() {
                 {/* {value.number} {value.englishName} {value.name}
                 {value.numberOfAyahs} {value.revelationType} */}
               </Paper>
+              <hr />
 
             </Grid>
 
           </div>
+          </Link>
+        
         )
       })
 
