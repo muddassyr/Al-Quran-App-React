7c1d80cdcde5c8a906f737b52612fa05c9f230a5
muddassirk <mkkhanu72@gmail.com>
2021-05-18 21:17:47 +0500

    aboutQuran component create

---
+++ src/components/AboutQuran/AboutQuran.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutQuran() {
+    return (
+        <div>
+            <h1>
+                About Quran
+            </h1>
+        </div>
+    )
+}
