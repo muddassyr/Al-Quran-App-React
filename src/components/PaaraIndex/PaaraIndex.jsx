ac5346913748c00d91862f6219efe1b54d3d1849
muddassirk <mkkhanu72@gmail.com>
2021-05-18 21:18:24 +0500

    paaraIndex component created

---
+++ src/components/PaaraIndex/PaaraIndex.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PaaraIndex() {
+    return (
+        <div>
+            <h1>
+                Paara
+            </h1>
+        </div>
+    )
+}
