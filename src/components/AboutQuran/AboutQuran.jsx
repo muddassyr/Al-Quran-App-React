ec2217cf24eac50df0b411c1c83aad57bd69fc80
muddassirk <mkkhanu72@gmail.com>
2021-05-20 15:40:22 +0500

    fetching data and display complete

--- src/components/AboutQuran/AboutQuran.jsx
+++ src/components/AboutQuran/AboutQuran.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -44,25 +43,38 @@ export default function AboutQuran() {
 
     useEffect(() => {
 
-        axios({
-            method: 'get',
-            url: 'http://api.alquran.cloud/v1/meta',//Meta - Get meta data about Surahs, Pages, Hizbs and Juzs
-        }).then((response) => {
-            // console.log(response, 'responese');
-            // console.log((response.data.data.ayahs.count));
-            // console.log((response.data.data.surahs));
-            setData(response.data.data)
+        // axios({
+        //     method: 'get',
+        //     url: 'http://api.alquran.cloud/v1/meta',//Meta - Get meta data about Surahs, Pages, Hizbs and Juzs
+        // }).then((response) => {
+        //     // console.log(response, 'responese');
+        //     // console.log((response.data.data));
+        //     setData(response.data.data)
+        // })
 
-        })
+
+        async function getData() {
+            const response = await fetch('http://api.alquran.cloud/v1/meta')
+
+            let datas = await response.json()
+            // console.log(datas.data);
+            delete datas.data.sajdas
+            setData(datas.data)
+            // console.log(data);
+
+
+        }
+        getData()
     }, [])
 
+    if (!data) return <h1>Loading...</h1>
 
 
     return (
         <div className={classes.root}>
 
             <Grid item xs={12}>
-                <Paper className={classes.paperBackGround} >About Quran</Paper>
+                <Paper className={classes.paperBackGround} >Information About Quran</Paper>
             </Grid>
             <hr />
 
@@ -74,28 +86,28 @@ export default function AboutQuran() {
                     const arr = []
                     arr.push(data.ayahs.count)
                     arr.push(data.surahs.count)
-                    arr.push(data.sajdas.count)
+                    // arr.push(data.sajdas.count)
                     arr.push(data.rukus.count)
                     arr.push(data.pages.count)
                     arr.push(data.manzils.count)
                     arr.push(data.hizbQuarters.count)
                     arr.push(data.juzs.count)
-                    console.log(arr);
+                    // console.log(arr);
                     return (
                         <div key={index}>
                             <Grid item xs={12}>
                                 <Paper className={classes.paper}>
                                     <div className={classes.mapInside}>
-                                        <h3>{value.replace(/_/g, ' ').toUpperCase()}</h3>
-                                            {arr.map((val, ind) =>{
-                                                return (
-                                                    <div key={ind}>
-                                                        <p>
-                                                            {val}
-                                                        </p>
-                                                    </div>
-                                                )
-                                            })}
+                                        <h4>How many {value.replace(/_/g, ' ').toUpperCase()} in Holy Quran ?</h4>
+                                        {arr.map((val, ind) => {
+                                            if (index === ind)
+                                                return <>
+                                                    <h4>{val}</h4>
+                                                </>
+                                            return (
+                                                <div key={ind}></div>
+                                            )
+                                        })}
                                     </div>
                                 </Paper>
                                 <hr />
