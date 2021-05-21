9bddfd6d183dc57b1c6a1721d792a6fd66600886
muddassirk <mkkhanu72@gmail.com>
2021-05-21 16:22:10 +0500

    minor

--- src/components/PaaraIndex/PaaraIndex.jsx
+++ src/components/PaaraIndex/PaaraIndex.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect  } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
@@ -42,7 +42,7 @@ export default function PaaraIndex() {
 
 
     const classes = useStyles();
- //   const [data, setData] = useState([])
+    //   const [data, setData] = useState([])
 
 
     useEffect(() => {
@@ -52,11 +52,13 @@ export default function PaaraIndex() {
             // url: 'http://api.alquran.cloud/v1/juz/{{juz}}/{{edition}}', // get /a juz
             // url: 'http://api.alquran.cloud/v1/juz/30/en.asad', // get a juz
             // url: 'http://api.alquran.cloud/v1/juz/1/{{edition}}',
-            url: 'http://api.alquran.cloud/v1/meta',//Meta - Get meta data about Surahs, Pages, Hizbs and Juzs
+            // url: 'http://api.alquran.cloud/v1/meta',//Meta - Get meta data about Surahs, Pages, Hizbs and Juzs
+            url: 'http://api.alquran.cloud/v1/quran',//complete quran
+
         }).then((response) => {
             console.log(response);
             // console.log(response.data.data.juzs);
-         //   setData(response.data.data.juzs)
+            //   setData(response.data.data.juzs)
         })
 
     }, [])
